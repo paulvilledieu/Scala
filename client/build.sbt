ef731a30e77a5b74bd695c37d65b4f220e0b7505
Christian Xu <christian.xu@epita.fr>
2019-05-09 11:10:37 +0200

    Client Update

--- client/build.sbt
+++ client/build.sbt
@@ -5,3 +5,4 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.4.1"
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.7.0"
--- client/src/main/scala/Sender.scala
+++ client/src/main/scala/Sender.scala
@@ -1,8 +1,42 @@
 import Request.sendMessage
-
+import play.api.libs.json._
 object Sender {
 
-  case class Message(m_type: Int, message: String)
+  case class Message(id: String,
+                     timestamp: Long,
+                     battery: Int,
+                     longitude: Double,
+                     latitude: Double,
+                     state: String,
+                     msg: String,
+                     hearth_rate: Float,
+                     something: Int,
+                     temperature: Float)
+
+  def csv_parser(str: String): Message  =  {
+    val msg = str.split(',')
+    create_message(msg)
+  }
+
+
+  def json_parser(str: String): Message = {
+
+  }
+
+  def create_message(params: Array[String]) = {
+    Message(
+      params.head,
+      params(1).toLong,
+      params(2).toInt,
+      params(3).toDouble,
+      params(4).toDouble,,
+      params(5),
+      params(6),
+      params(7).toFloat,
+      params(8).toInt,
+      params(9).toFloat)
+  }
+
 
   def send_msg(msg: Message) = {
     //post traitements des différents types de messages
